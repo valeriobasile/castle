31317dbadca4cddf87cf0dd2c46526d90cefab47
Valerio Basile <valeriobasile@gmail.com>
2018-05-02 19:16:37 +0200

    changed the test output

--- src/test.py
+++ src/test.py
@@ -4,6 +4,9 @@ from optparse import OptionParser
 import numpy as np
 from vector import VectorPairReader
 from scipy.spatial.distance import cosine
+from os.path import isfile
+
+log.basicConfig(level=log.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S')
 
 # command line argument partsing
 parser = OptionParser()
@@ -11,6 +14,11 @@ parser.add_option('-i',
                   '--input',
                   dest="input_file",
                   help="read training vector pairs from FILE",
+                  metavar="FILE")
+parser.add_option('-l',
+                  '--labels',
+                  dest="label_file",
+                  help="read label pairs from FILE",
                   metavar="FILE")
 parser.add_option('-t',
                   '--tensor',
@@ -20,8 +28,13 @@ parser.add_option('-t',
                   metavar="FILE")
 (options, args) = parser.parse_args()
 
-if options.input_file:
+if options.input_file and isfile(options.input_file):
     input_file = options.input_file
+else:
+    parser.usage()
+    sys.exit(1)
+if options.label_file and isfile(options.label_file):
+    label_file = options.label_file
 else:
     parser.usage()
     sys.exit(1)
@@ -40,6 +53,12 @@ for v1, v2 in dr:
 dimensions = len(testing_pairs[0][0])
 testing_data = [(1, v1, v2) for v1, v2 in testing_pairs]
 
+testing_labels = []
+log.info ("reading label file {0}".format(label_file))
+with open(label_file) as f:
+    for line in f:
+      testing_labels.append(line.strip().split('\t'))
+
 tensor = []
 log.info ("reading tensor file {0}".format(tensor_file))
 with open(tensor_file) as f:
@@ -48,10 +67,12 @@ with open(tensor_file) as f:
 
 log.info ("testing")
 avg_dist = 0.0
-for sign, h, t in testing_data:
+for i, (sign, h, t) in enumerate(testing_data):
     x = np.dot(tensor,h)
-    avg_dist += cosine(t, x)
+    dist = cosine(t, x)
+    print ("{0:.3f} {1} {2}".format(dist, testing_labels[i][0], testing_labels[i][1]))
+    avg_dist += dist
 
-print ("mean cosine distance: {0}".format(avg_dist/len(testing_data)))
-
+avg_dist = avg_dist/len(testing_data)
+#print ("mean cosine distance: {0}".format(avg_dist))
 
